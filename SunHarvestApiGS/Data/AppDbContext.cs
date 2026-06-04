c9096cde584a6f21e8738f985571e46e392d1dac
laura853 <lauralopess2006@gmail.com>
2026-06-04 14:27:36 -0300

    configurando o banco de dados e a documentacao

--- SunHarvestApiGS/Data/AppDbContext.cs
+++ SunHarvestApiGS/Data/AppDbContext.cs
@@ -1,6 +1,30 @@
-﻿namespace SunHarvestApiGS.Data
+﻿using Microsoft.EntityFrameworkCore;
+using SunHarvestApiGS.Models;
+
+namespace SunHarvestApiGS.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public DbSet<Fazenda> Fazendas { get; set; }
+
+        public DbSet<Alerta> Alertas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Nomes das tabelas no Oracle 
+
+            modelBuilder.Entity<Usuario>().ToTable("TB_USUARIO");
+            modelBuilder.Entity<Fazenda>().ToTable("TB_FAZENDA");
+            modelBuilder.Entity<Alerta>().ToTable("TB_ALERTA");
+
+            //Email do usuário é único
+
+            modelBuilder.Entity<Usuario>().HasIndex(u => u.Email).IsUnique();
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
--- SunHarvestApiGS/Program.cs
+++ SunHarvestApiGS/Program.cs
@@ -1,23 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+using SunHarvestApiGS.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("OracleConnection");
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//Configura o DbContext para usar o Oracle
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseOracle(connectionString, b => b.UseOracleSQLCompatibility(OracleSQLCompatibility.DatabaseVersion19));
+});
+
+builder.Services.AddControllers()
+     .AddJsonOptions(options =>
+     {
+         options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+     });
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddOpenApi(options =>
+{
+    options.AddDocumentTransformer((document, context, cancellationToken) => {
+
+        document.Info.Title = "SunHarvest Agro - Sistema Inteligente de Irrigação Agropecuária";
+        document.Info.Version = "v1";
+        document.Info.Description = "API para gerenciamento de fazendas, alertas e usuários do sistema inteligente de irrigação agropecuária.";
+        return Task.CompletedTask;
+    });
+
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
